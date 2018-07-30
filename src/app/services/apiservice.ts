92b7884b85078c0c83ad9f25de5fecdd54e6a898
Your Name <you@example.com>
2018-07-30 17:07:36 +0530

    update user register

---
+++ src/app/services/apiservice.ts
@@ -0,0 +1,43 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { environment } from "../../environments/environment";
+@Injectable()
+export class ApiService {
+  constructor(private http: HttpClient) {}
+
+  postlogin(post) {
+    const params = new HttpParams()
+      .set("username", post.email)
+      .set("password", post.password);
+    return new Promise((resolve, reject) => {
+      this.http
+        .get(`${environment["apiBase"]}login`, {
+          params: params
+        })
+        .subscribe(data => {
+          if (data["error"] && data["error"] === 1) {
+            resolve(data);
+          } else {
+            reject(data);
+          }
+        });
+    });
+  }
+  postregister(post) {
+    const params = new HttpParams()
+      .set("username", post.email)
+      .set("password", post.password)
+      .set("role", post.role);
+    return new Promise((resolve, reject) => {
+      this.http
+        .get(`${environment["apiBase"]}add_user`, { params: params })
+        .subscribe(data => {
+          if (data["error"] && data["error"] === 1) {
+            resolve(data);
+          } else {
+            reject(data);
+          }
+        });
+    });
+  }
+}
