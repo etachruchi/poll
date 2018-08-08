80616074e11f8d283f6b2aeee2a394b1e73ec08b
Your Name <you@example.com>
2018-08-08 19:38:51 +0530

    vote update

--- src/app/Component/login/login.component.ts
+++ src/app/Component/login/login.component.ts
@@ -46,7 +46,6 @@ export class LoginComponent implements OnInit {
       .catch(err => {
         this.loading = false;
         this.errorMessage = err.message;
-        console.log(this.errorMessage);
       });
   
   }
--- src/app/Component/vote-poll/vote-poll.component.ts
+++ src/app/Component/vote-poll/vote-poll.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
-import { Router, ActivatedRoute } from "@angular/router";
 import { ApiService } from "../../services/apiservice";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
-import * as _ from "lodash";
 @Component({
   selector: "app-vote-poll",
   templateUrl: "./vote-poll.component.html",
@@ -12,7 +10,7 @@ export class VotePollComponent implements OnInit {
   @Output() updatePoll = new EventEmitter();
   @Output() deletePoll = new EventEmitter();
   submitted: boolean;
-  @Input() parentData;
+  @Input() pollData;
   id: number;
   opt_id: string;
   Pollid: string;
@@ -20,38 +18,23 @@ export class VotePollComponent implements OnInit {
   selectedPoll = false;
   constructor(
     private apiServices: ApiService,
-    private router: Router,
-    private activatedRoute: ActivatedRoute
   ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   deleteOption(id, opt_id) {
-    _.remove(this.parentData.options, { opt_id: opt_id });
+    _.remove(this.pollData.options, { opt_id: opt_id });
     this.apiServices.deleteOption(id, opt_id).subscribe(res => {});
   }
   deletePollfun(id) {
-    this.deletePoll.emit(this.parentData.id);
+    this.deletePoll.emit(this.pollData.id);
   }
   updatePollfunction(id) {
-    this.updatePoll.emit(this.parentData.id);
+    this.updatePoll.emit(this.pollData.id);
   }
   onSubmit(opt_id) {
-    this.apiServices.vote(this.parentData.id, opt_id.poll).subscribe(res => {
+    this.apiServices.vote(this.pollData.id, opt_id.poll).subscribe(res => {
       this.updatePoll.emit(res["data"]);
-
     });
-  
-  }
-
-  isDisabled() {
-    if(_.indexOf(this.pollsArray,this.parentData.id) != -1) {
-      this.submitted = true;
-      return true;
-    } else {
-      this.submitted = false;
-      return false;
-    }
   }
 }
